7be992e3de41b0525903d2c190af4d3d2f231032
fezeboris <borisfeze9@gmail.com>
2022-08-16 11:07:49 +0100

    form validation and sql database file added

--- app/app.js
+++ app/app.js
@@ -1,33 +1,32 @@
-fetch("http://localhost:3000/products")
+fetch('http://localhost:3000/products')
   .then((response) => response.json())
   .then((data) => {
-    let pizzas = data;
-    console.log(pizzas);
+    let pizzas = data
+    // console.log(pizzas)
     pizzas.forEach((p) => {
-      let pizza = document.createElement("div");
-      pizza.className = "chicken-burgerr";
-      let pizzaImg = document.createElement("img");
-      pizzaImg.src = `../api-pizza/images/${p.img}`;
-      pizza.appendChild(pizzaImg);
-      let pizzaTitle = document.createElement("p");
-      pizzaTitle.innerHTML = p.title;
-      pizza.appendChild(pizzaTitle);
-      let pizzaPriceContainer = document.createElement("div");
-      pizzaPriceContainer.className = "burger-price";
-      let pizzaPrice = document.createElement("div");
-      pizzaPrice.className = "burger-price-details";
-      pizzaPrice.innerHTML = p.price;
-      pizzaPriceContainer.appendChild(pizzaPrice);
-      let pizzaBtn = document.createElement("div");
-      pizzaBtn.className = "price-btn";
-      pizzaBtn.innerHTML = "Add to cart";
-      pizzaPriceContainer.appendChild(pizzaBtn);
-      pizza.appendChild(pizzaPriceContainer);
+      let pizza = document.createElement('div')
+      pizza.className = 'chicken-burgerr'
+      let pizzaImg = document.createElement('img')
+      pizzaImg.src = `../api-pizza/images/${p.img}`
+      pizza.appendChild(pizzaImg)
+      let pizzaTitle = document.createElement('p')
+      pizzaTitle.innerHTML = p.title
+      pizza.appendChild(pizzaTitle)
+      let pizzaPriceContainer = document.createElement('div')
+      pizzaPriceContainer.className = 'burger-price'
+      let pizzaPrice = document.createElement('div')
+      pizzaPrice.className = 'burger-price-details'
+      pizzaPrice.innerHTML = p.price
+      pizzaPriceContainer.appendChild(pizzaPrice)
+      let pizzaBtn = document.createElement('div')
+      pizzaBtn.className = 'price-btn'
+      pizzaBtn.innerHTML = 'Add to cart'
+      pizzaPriceContainer.appendChild(pizzaBtn)
+      pizza.appendChild(pizzaPriceContainer)
       let pizzaContainer = document
-        .getElementById("chicken-containerr")
-        .appendChild(pizza);
-      console.log(pizza);
-    });
-  });
+        .getElementById('chicken-containerr')
+        .appendChild(pizza)
+    // console.log(pizza)
+    })
+  })
 
-console.log("helloworld");
---
+++ app/login.js
@@ -0,0 +1,49 @@
+let lname = document.getElementById('lname')
+let fname = document.getElementById('fname')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let confPassword = document.getElementById('confPassword')
+let spassword
+let townVal
+
+let submit = document.getElementById('submit')
+submit.addEventListener('click', function (event) {
+  spassword = password.value
+
+  event.preventDefault()
+  if (lname.value == '') {
+    let err = document.getElementById('lnameError')
+    err.innerHTML = 'Invalid surname'
+    setTimeout(() => {
+      err.innerHTML = ''
+    }, 5000)
+  } else if (fname.value == '') {
+    let err = document.getElementById('fnameError')
+    err.innerHTML = 'Invalid Name'
+    setTimeout(() => {
+      err.innerHTML = ''
+    }, 5000)
+  }
+  else if (spassword.length < 8) {
+    let emailErr = document.getElementById('emailErr')
+    emailErr.innerHTML = 'Password must be more than 8 characters'
+    setTimeout(() => {
+      emailErr.innerHTML = ''
+    }, 5000)
+  } else if (spassword != confPassword.value) {
+    let conpassErr = document.getElementById('conpassErr')
+    conpassErr.innerHTML = 'password is not the same as confirm password'
+    setTimeout(() => {
+      conpassErr.innerHTML = ''
+    }, 5000)
+  } else {
+    let radioSection = document.getElementsByName('town')
+
+    for (let i = 0; i < radioSection.length; i++) {
+      if (radioSection[i].checked) {
+        townVal = radioSection[i].value
+        console.log(townVal)
+      }
+    }
+  }
+})
