7b1cbfbd85e74736106481b83c8f921f0523b671
fezeboris <borisfeze9@gmail.com>
2022-08-16 14:43:57 +0100

    setting timeout

--- app/login.js
+++ app/login.js
@@ -1,49 +1,25 @@
-let lname = document.getElementById('lname')
-let fname = document.getElementById('fname')
-let email = document.getElementById('email')
-let password = document.getElementById('password')
-let confPassword = document.getElementById('confPassword')
-let spassword
-let townVal
+let loginEmail = document.getElementById('loginEmail')
+let loginPassword = document.getElementById('loginPassword')
 
-let submit = document.getElementById('submit')
-submit.addEventListener('click', function (event) {
-  spassword = password.value
-
-  event.preventDefault()
-  if (lname.value == '') {
-    let err = document.getElementById('lnameError')
-    err.innerHTML = 'Invalid surname'
-    setTimeout(() => {
-      err.innerHTML = ''
-    }, 5000)
-  } else if (fname.value == '') {
-    let err = document.getElementById('fnameError')
-    err.innerHTML = 'Invalid Name'
-    setTimeout(() => {
-      err.innerHTML = ''
-    }, 5000)
-  }
-  else if (spassword.length < 8) {
-    let emailErr = document.getElementById('emailErr')
-    emailErr.innerHTML = 'Password must be more than 8 characters'
-    setTimeout(() => {
-      emailErr.innerHTML = ''
-    }, 5000)
-  } else if (spassword != confPassword.value) {
-    let conpassErr = document.getElementById('conpassErr')
-    conpassErr.innerHTML = 'password is not the same as confirm password'
-    setTimeout(() => {
-      conpassErr.innerHTML = ''
-    }, 5000)
-  } else {
-    let radioSection = document.getElementsByName('town')
-
-    for (let i = 0; i < radioSection.length; i++) {
-      if (radioSection[i].checked) {
-        townVal = radioSection[i].value
-        console.log(townVal)
-      }
+function handleSubmit(event){
+    event.preventDefault()
+    if (loginEmail.value == "") {
+        let logparaErr = document.getElementById('logparaErr')
+        logparaErr.innerHTML = 'Email feild empty'
+        setTimeout(() => {
+            logparaErr.innerHTML = ''
+          }, 5000)
+    } else if (loginPassword.value == "") {
+        let logpassErr = document.getElementById('logpassErr')
+        logpassErr.innerHTML = 'Invalid password'
+        setTimeout(() => {
+            logpassErr.innerHTML = ''
+          }, 5000)
+    } else {
+        console.log('Email:',loginEmail.value, )
+        console.log('Password:', loginPassword.value,)
+        loginEmail.value = ''
+        loginPassword.value = ''
     }
-  }
-})
+
+}
